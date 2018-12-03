7a5e24a96a5ff5949ec704ef435d7ea8ddd6716a
ThanhTung1235 <tungltd00602@fpt.edu.vn>
2018-12-03 17:44:42 +0700

    wheather cotent

--- Js/Script.js
+++ Js/Script.js
@@ -1,21 +1,48 @@
-window.onload = hi();
-function hi() {
-    var btn_right = document.getElementById("right");
-    var btn_left = document.getElementById("left");
-    var scroll = document.getElementById("scroll");
-    btn_right.onclick = function () {
-        scroll.scrollTo(1000, 0);
-        btn_right.classList.add("d-none");
-        btn_left.classList.remove("d-none");
+var btn_right = document.getElementById("right");
+var btn_left = document.getElementById("left");
+var scroll = document.getElementById("scroll");
+var wheather = document.getElementsByClassName("btn-wheather");
+var hi = document.getElementById("hi");
+var close = document.getElementById("close");
+for (var i = 0; i < wheather.length; i++) {
+    wheather[i].onclick = function () {
+        hi.classList.remove("d-none");
+    }
+}
+close.onclick =function () {
+    hi.classList.add("d-none");
+};
 
-    };
-
-    btn_left.onclick = function () {
-        scroll.scrollTo(0, 0);
+function myFunction() {
+    btn_left.classList.remove("d-none");
+    var a = scroll.scrollLeft;
+    // console.log(a);
+    // console.log(scroll.scrollWidth);
+    // console.log(scroll.offsetWidth);
+    if (scroll.scrollLeft == 0) {
         btn_right.classList.remove("d-none");
         btn_left.classList.add("d-none");
     }
+    else if (scroll.offsetWidth > a && 0 < a) {
+        btn_right.classList.remove("d-none");
+        btn_left.classList.remove("d-none");
+    }
+    if (scroll.scrollWidth == a + scroll.offsetWidth) {
+        btn_right.classList.add("d-none")
+    }
 }
+
+btn_right.onclick = function () {
+    scroll.scrollTo(scroll.scrollWidth, 0);
+    btn_right.classList.add("d-none");
+    btn_left.classList.remove("d-none");
+};
+
+btn_left.onclick = function () {
+    scroll.scrollTo(0, 0);
+    btn_right.classList.remove("d-none");
+    btn_left.classList.add("d-none");
+};
 
 var nav = document.getElementById("navbar");
 var sticky = nav.offsetTop;
